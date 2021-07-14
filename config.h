8ed09b6b16ae1738989092b23635b6981db9f57d
Bruno Montezano <brunodoyt@gmail.com>
2021-07-14 00:54:20 -0300

    New colorscheme.

--- config.h
+++ config.h
@@ -103,30 +103,30 @@ unsigned int alpha = 0xff;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#3b4252", /* black   */
-	"#bf616a", /* red     */
-	"#a3be8c", /* green   */
-	"#ebcb8b", /* yellow  */
-	"#81a1c1", /* blue    */
-	"#b48ead", /* magenta */
-	"#88c0d0", /* cyan    */
-	"#e5e9f0", /* white   */
+	"#15161e", /* black   */
+	"#f7768e", /* red     */
+	"#9ece6a", /* green   */
+	"#e0af68", /* yellow  */
+	"#7aa2f7", /* blue    */
+	"#bb9af7", /* magenta */
+	"#7dcfff", /* cyan    */
+	"#a9b1d6", /* white   */
 
 	/* 8 bright colors */
-	"#4c566a", /* black   */
-	"#bf616a", /* red     */
-	"#a3be8c", /* green   */
-	"#ebcb8b", /* yellow  */
-	"#81a1c1", /* blue    */
-	"#b48ead", /* magenta */
-	"#8fbcbb", /* cyan    */
-	"#eceff4", /* white   */
+	"#414868", /* black   */
+	"#f7768e", /* red     */
+	"#9ece6a", /* green   */
+	"#e0af68", /* yellow  */
+	"#7aa2f7", /* blue    */
+	"#bb9af7", /* magenta */
+	"#7dcfff", /* cyan    */
+	"#c0caf5", /* white   */
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#2e3440", /* background */
-	"#d8dee9", /* foreground */
+	"#1a1b26", /* background */
+	"#c0caf5", /* foreground */
  };
  
 /*
